052e7b52b6c55d7db053705f3ba2c7e7fab10436
Meghan Stang <mstang15@gmail.com>
2018-09-01 12:16:49 -0600

    adds display_board with empty spaces

---
+++ lib/board.rb
@@ -0,0 +1,22 @@
+
+
+class Board
+
+  def initialize
+    @grid = empty_playing_grid
+  end
+
+  def empty_playing_grid
+    {"A" => [Space.new, Space.new, Space.new, Space.new],
+    "B" => [Space.new, Space.new, Space.new, Space.new],
+    "C" => [Space.new, Space.new, Space.new, Space.new],
+    "D" => [Space.new, Space.new, Space.new, Space.new]}
+  end
+
+  def display_board
+    puts "==========="
+    puts ". 1 2 3 4"
+    puts "A #{@grid["A"]}\n B #{@grid["B"]}\n C #{@grid["C"]}\n D #{@grid["C"]}"
+  end
+
+end
---
+++ test/board_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/space'
+require './lib/board'
+require 'pry'
+
+class BoardTest <Minitest::Test
+  def test_it_exists
+    b = Board.new
+
+    assert_instance_of Board, b
+  end
+
+
+end
