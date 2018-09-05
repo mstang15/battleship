c0bd34b72a470757bcd3ce8c8f7149e909f6b784
Meghan Stang <mstang15@gmail.com>
2018-09-05 15:35:43 -0600

    combined methods in runner file

--- lib/game.rb
+++ lib/game.rb
@@ -19,6 +19,7 @@ class Game
   end
 
   def intro
+    puts `clear`
     puts "\n\nWelcome to BATTLESHIP\n\nWould you like to (p)lay, read the (i)nstructions, or (q)uit?"
     initial_choice = user_input_downcase
     start_game_options(initial_choice)
@@ -52,11 +53,11 @@ class Game
   def player_ship_placement
     player_ship_placement_instructions_1
     players_destroyer = get_player_destroyer
-    place_players_destroyer(players_destroyer)
+    @player.player_place_destroyer(players_destroyer, @player_board.grid)
     puts "\n\nNow enter the squares for the three-unit ship:"
     players_cruiser = get_player_cruiser
-    cruiser = place_players_cruiser(players_cruiser)
-    store_player_ships(players_destroyer, cruiser)
+    cruiser = @player.player_place_cruiser(players_cruiser,@player_board.grid)
+    @validate.store_player_ships(players_destroyer,cruiser)
     user_input_enter
     puts `clear`
     begin_game_flow
@@ -77,19 +78,7 @@ class Game
     players_cruiser = user_input_upcase
     validate_cruiser = @validate.validate_cruiser_placement(players_cruiser,@player_board.grid)
     end
-    players_cruiser
-  end
-
-  def place_players_destroyer(players_destroyer)
-    @player.player_place_destroyer(players_destroyer, @player_board.grid)
-  end
-
-  def place_players_cruiser(players_cruiser)
-    @player.player_place_cruiser(players_cruiser.split,@player_board.grid)
-  end
-
-  def store_player_ships(players_destroyer,players_cruiser)
-    @validate.store_player_ships(players_destroyer,players_cruiser)
+    players_cruiser.split
   end
 
   def begin_game_flow
@@ -103,9 +92,8 @@ class Game
   def player_shot_sequence
     @count_turns += 1
     guess = retrieve_player_guess
-
     puts `clear`
-    store_player_guess(guess)
+    @validate.store_player_guesses(guess,@computer_board.grid)
     display_both_boards
     hit_or_miss = @validate.hit_or_miss(guess,@computer_board.grid)
     won = check_if_someone_won(@computer_board.grid)
@@ -117,16 +105,6 @@ class Game
     computer_shot_sequence
   end
 
-  def player_wins
-    puts "\n\nCONGRATULATIONS! You won this round of battleship.\nThis game took us #{calculate_time_difference[0]} minutes and #{calculate_time_difference[1]} seconds to play, and you won in #{@count_turns} turns!\n\n"
-    quit
-  end
-
-  def computer_wins
-    puts "\n\nI WIN! Better luck next time!\nThis game took us #{calculate_time_difference[0]} minutes and #{calculate_time_difference[1]} seconds to play, and I won in #{@count_turns} turns!\n\n"
-    quit
-  end
-
   def retrieve_player_guess
     player_guess = false
     while player_guess != true
@@ -135,40 +113,19 @@ class Game
         quit
         return true
       else
-        player_guess = validate_player_guess(guess)
+        player_guess = @validate.validate_guess(guess)
       end
     end
     return guess
   end
 
-  def validate_player_guess(guess)
-    @validate.validate_guess(guess)
-  end
-
-  def store_player_guess(guess)
-    @validate.store_player_guesses(guess,@computer_board.grid)
-  end
-
   def computer_shot_sequence
-    guess = retrieve_computer_guess
+    guess = @ships.generate_random_guess
     puts `clear`
-    store_computer_guess(guess)
-    hit_or_miss = record_hit_or_miss(guess)
-
+    @validate.store_computer_guesses(guess,@player_board.grid)
+    hit_or_miss = @validate.hit_or_miss(guess,@player_board.grid)
     computer_shot_sequence_output(guess,hit_or_miss)
     player_shot_sequence
-  end
-
-  def retrieve_computer_guess
-    @ships.generate_random_guess
-  end
-
-  def store_computer_guess(guess)
-    @validate.store_computer_guesses(guess,@player_board.grid)
-  end
-
-  def record_hit_or_miss(guess)
-    @validate.hit_or_miss(guess,@player_board.grid)
   end
 
   def computer_shot_sequence_output(guess,hit_or_miss)
@@ -197,15 +154,13 @@ class Game
     @computer_board.display_board
   end
 
-  def quit
-    puts "Thanks for playing.\nGoodbye."
-    exit!
-  end
-
   def instructions
     puts `clear`
     instructions = File.new('./lib/instructions.txt', 'r').read
     puts instructions
+    puts "Press enter to go back to welcome screen."
+    user_input_enter
+    puts `clear`
     intro
   end
 
@@ -234,8 +189,24 @@ class Game
     print ">"
     enter = gets
     while enter != "\n"
-      puts " Please press enter so I can have my turn."
+      puts "Please press enter."
       enter = gets
     end
   end
+  
+  def player_wins
+    puts "\n\nCONGRATULATIONS! You won this round of battleship.\nThis game took us #{calculate_time_difference[0]} minutes and #{calculate_time_difference[1]} seconds to play, and you won in #{@count_turns} turns!\n\n"
+    quit
+  end
+
+  def computer_wins
+    puts "\n\nI WIN! Better luck next time!\nThis game took us #{calculate_time_difference[0]} minutes and #{calculate_time_difference[1]} seconds to play, and I won in #{@count_turns} turns!\n\n"
+    quit
+  end
+
+  def quit
+    puts "Thanks for playing.\nGoodbye.\n\n"
+    exit!
+  end
+
 end
